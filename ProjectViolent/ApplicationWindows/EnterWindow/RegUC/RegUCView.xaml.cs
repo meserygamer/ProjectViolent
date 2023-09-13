6f0fafc9de8b3f2b14ccb4fba9d58297a61b7bbf
Mesery <Meserygamer@gmail.com>
2023-09-13 17:08:19 +0300

    Оформлен макет окна авторизации

---
+++ ProjectViolent/ApplicationWindows/EnterWindow/RegUC/RegUCView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectViolent.ApplicationWindows.EnterWindow.RegUC
+{
+    /// <summary>
+    /// Логика взаимодействия для RegUCView.xaml
+    /// </summary>
+    public partial class RegUCView : UserControl
+    {
+        public RegUCView()
+        {
+            InitializeComponent();
+        }
+    }
+}
