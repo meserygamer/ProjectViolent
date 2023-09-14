bbf4601c8ef33b544e487c7a2b0b33f7893f43ea
Mesery <Meserygamer@gmail.com>
2023-09-14 23:32:02 +0300

    Сделан Не доделан ViewModel

---
+++ ProjectViolent/ApplicationWindows/EnterWindow/SetPersonalDataUC/SetPersonalDataUCViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectViolent.ApplicationWindows.EnterWindow.SetPersonalDataUC
+{
+    public class SetPersonalDataUCViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _surName;
+        private string _patronymic;
+        private DateTime _birthday;
+        private int _idSelectedGender;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string SurName
+        {
+            get => _surName;
+            set
+            {
+                _surName = value;
+                OnPropertyChanged("SurName");
+            }
+        }
+
+        public string Patronymic
+        {
+            get => _patronymic;
+            set
+            {
+                _patronymic = value;
+                OnPropertyChanged("Patronymic");
+            }
+        }
+
+        public DateTime Birthday
+        {
+            get => _birthday;
+            set
+            {
+                _birthday = value;
+                OnPropertyChanged("Birthday");
+            }
+        }
+
+        public int IdSelectedGender
+        {
+            get => _idSelectedGender;
+            set
+            {
+                _idSelectedGender = value;
+                OnPropertyChanged("IdSelectedGender");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
