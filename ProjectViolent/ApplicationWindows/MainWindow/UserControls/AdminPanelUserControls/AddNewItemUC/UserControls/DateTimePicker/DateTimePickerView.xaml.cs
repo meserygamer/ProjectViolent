9831e888cf8f35fe6e4826f091157005b5c815d1
Meserygamer <meserygamer@gmail.com>
2023-10-10 16:38:28 +0300

    Заготовка под Выбор даты

---
+++ ProjectViolent/ApplicationWindows/MainWindow/UserControls/AdminPanelUserControls/AddNewItemUC/UserControls/DateTimePicker/DateTimePickerView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectViolent.ApplicationWindows.MainWindow.UserControls.AdminPanelUserControls.AddNewItemUC.UserControls.DateTimePicker
+{
+    /// <summary>
+    /// Логика взаимодействия для DateTimePickerView.xaml
+    /// </summary>
+    public partial class DateTimePickerView : UserControl
+    {
+        public DateTimePickerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
